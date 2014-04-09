4ff7c20d2ebc4c7541b74ad28fca7b6211e24e9a
caedus75 <carlos4735@gmail.com>
2014-04-08 23:31:41 -0300

    codigo reescrito

--- Desafio_1/caedus75/caesar.py
+++ Desafio_1/caedus75/caesar.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env  python3
 
+#   chr() retorna o caractere __unicode__ correspondente ao inteiro passado como
+#   argumento. Sendo assim não há o limite de 255 do ascii. É possível modificar
+#   todas as letras, acentuadas ou não, da frase.
+def modify(original, key):
+    mod = []
+    for char in original:
+        mod.append(chr(ord(char)+key))
+    return ''.join(mod)
+
 frase = input("Digite a frase: ")
-cifra = []
-
-#   Lista de caracteres que serao ignorados na criptografia.
-ign = ['Á', 'á', 'Â', 'â', 'À', 'à', 'Å', 'å', 'Ã', 'ã', 'Ä', 'ä',\
-       'É', 'é', 'Ê', 'ê', 'È', 'è', 'Ë', 'ë', 'Í', 'í', 'Î', 'î',\
-       'Ì', 'ì', 'Ï', 'ï', 'Ó', 'ó', 'Ô', 'ô', 'Ò', 'ò', 'Õ', 'õ',\
-       'Ö', 'ö', 'Ú', 'ú', 'Û', 'û', 'Ù', 'ù', 'Ü', 'ü', 'Ç', 'ç',\
-       'Ñ', 'ñ', '\"', '\'', '®', '©', '<', '>', '&', ' ']
-
 #   Garante que a chave numerica seja realmente um numero
 while True:
     try:
@@ -23,20 +23,8 @@ while True:
 while key > 25:
     key -= 26
 
-#   Usa a frase como uma sequencia de caracteres.
-#   a cada caracter verifica se está presente na lista de caracteres a serem
-#   ignorados (ign), se sim so adiciona a uma lista de caracteres sem fazer mais
-#   nada.
-#   Se nao, transforma o caracter no seu valor numerico unicode e soma a chave.
-#   O novo valor é transformado de volta em caracter e adicionado a lista de
-#   caracteres.
-for char in frase:
-    if char in ign:
-        cifra.append(char)
-        continue
-    cifra.append(chr(ord(char)+key))
+#   embaralha e desembaralha a frase, respectivamente.
+enc = modify(frase, key)
+dec = modify(enc, key*-1)
 
-#   Cada elemento da lista é unido com um espaco vazio entre si, tecninca
-#   gatinho para fazer uma string a partir de uma lista de caracteres.
-cifra = ''.join(cifra)
-print(cifra)
+print("frase: {0}\ncifrado: {1}\ndecifrado: {2}".format(frase, enc, dec))
