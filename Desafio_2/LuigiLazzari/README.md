d78bbe6d8c09b9c8f4df29f7b4f0978985f173db
LuigiLazzari <luigi.lc89@gmail.com>
2014-04-14 18:45:00 -0300

    adicionando comeco desafio2

---
+++ Desafio_2/LuigiLazzari/README.md
@@ -0,0 +1,27 @@
+# Sudoku 
+Pra rodar vc vai precisar ter Lua 5.1 instalado no seu pc, ou vc pode usar a versão de testes(já que ngm tem lua instalado :/) 'sudoku-demo.lua' no site: [Lua Demo](http://www.lua.org/cgi-bin/demo) Só copiar e colar o código e dar um Run pra vc ver o resultado.
+
+
+## Sudoku
+
+
+Rodando a Demo:
+Altere a grade no início:
+
+    sudo = "1 2 3 4 5 6 7 8 9" .. -- o .. significa concatenação de strings
+           "4 5 6 7 8 9 1 2 3" ..
+           "7 8 9 1 2 3 4 5 6" ..
+           "8 9 1 2 3 4 5 6 7" ..
+           "2 3 4 5 6 7 8 9 1" ..
+           "5 6 7 8 9 1 2 3 4" ..
+           "6 7 8 9 1 2 3 4 5" ..
+           "9 1 2 3 4 5 6 7 8" ..
+           "3 4 5 6 7 8  9 1 2"
+
+
+
+## Versão completa
+Pra rodar o arquivo baixe a versão lua 5.1 que vem com o SciTe que é um editor que compila lua.
+
+
+## Não tá pronto :~
---
+++ Desafio_2/LuigiLazzari/sudoku-demo.lua
@@ -0,0 +1,26 @@
+sudo="1 2 3 4 5 6 7 8 9" ..
+     "4 5 6 7 8 9 1 2 3" ..
+     "7 8 9 1 2 3 4 5 6" ..
+     "8 9 1 2 3 4 5 6 7" ..
+     "2 3 4 5 6 7 8 9 1" ..
+     "5 6 7 8 9 1 2 3 4" ..
+     "6 7 8 9 1 2 3 4 5" ..
+     "9 1 2 3 4 5 6 7 8" ..
+     "3 4 5 6 7 8 9 1 2" --deixando bonito para testes, inicializando o sudoku com um que é válido
+
+tables = {
+    {}, {}, {} -- inicializando 1 tabela para linhas, colunas e para cada grade 3x3, respectivamente
+} 
+
+for i=1,3 do -- k=1, 3 pq é o número de tabelas em tables
+    for j=1,9 do 
+        tables[i][j]={0,0,0,0,0,0,0,0,0} -- colocar 0's em cada uma das tabelas em tables para evitar nils. Cada tabela tem 9 elementos :D 
+    end
+end 
+
+for i=1,81 do -- loop por todos os números
+    n=tonumber(sudo:sub(i,i):match'%d') --[[ pega o caractere, checa se é um número com match'%d', 
+                                             e converte para número com sub(k,k), pq está td inicializado como string. ]]--
+    if n then
+
+      --conta foda a ser implementada hu3
