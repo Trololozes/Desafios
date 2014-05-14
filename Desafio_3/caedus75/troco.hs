31cb2c16b77514df6ab00776ec14f4b30ee96a6b
caedus75 <carlos4735@gmail.com>
2014-05-14 02:33:30 -0300

    refatoradas funcoes

--- Desafio_3/caedus75/troco.hs
+++ Desafio_3/caedus75/troco.hs
@@ -12,10 +12,8 @@ troco :: Double -> Double -> Double
 troco valor dinheiro
     | valor >= dinheiro = 0
     | otherwise = (fromInteger $ fx $ round $ (dinheiro - valor) * 100) / 100
-        where
-            fx a = if (mod a 5 /= 0)
-                then fx (a+1)
-                else a
+    where
+        fx a = if (mod a 5 /= 0) then fx (a+1) else a
 
 --  Retorna uma lista de quantidades de cédulas/moedas necessárias para igualar
 --  o valor passado.
@@ -26,13 +24,9 @@ moeda xs y = calc xs y 0
     where
         calc [] _ _ = []
         calc (x:xs) y n
-            | y >= x = calc (x:xs) (myRound $ y-x) (n+1)
+            | y >= x = calc (x:xs) (rnd $ y-x) (n+1)
             | otherwise = n : calc xs y 0
-
-dropZero :: (Double, Int) -> Bool
-dropZero (_, x)
-    | x == 0 = False
-    | otherwise = True
+        rnd x = (fromInteger $ round $ x * 100) / 100.0
 
 --  Usa a lista de duplas (Valor, Qtdd) para gerar uma lista de Strings
 --  formatada no modo de apresentação final.
@@ -41,15 +35,16 @@ str xs = map (\(a, b) -> concat [(show b), "x R$ ", (bty a)]) xs
     where
         bty a = printf "%.2f" a :: String
 
-myRound :: Double -> Double
-myRound x = (fromInteger $ round $ x * (10^2)) / (10.0^^2)
-
 --  Função 'empacota' todo o processo de calcular o troco, gerar a lista de
 --  quantidades, unir a lista com a lista de valores, e descartar os valores que
 --  não são usados numa coisa só.
 process :: Double -> Double -> [(Double, Int)]
 process x y =
     filter dropZero $ zip reais $ moeda reais $ troco x y
+    where
+        dropZero (_, x)
+            | x == 0 = False
+            | otherwise = True
 
 --  Função main.
 --  Faz a interação com o mundo exterior
