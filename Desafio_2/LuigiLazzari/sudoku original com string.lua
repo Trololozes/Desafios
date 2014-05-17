775d2bbd159252ffbdee3e83b575e7de5e04ff3d
LuigiLazzari <luigi.lc89@gmail.com>
2014-05-16 22:41:55 -0300

    refatoracao desafio 3

---
+++ Desafio_2/LuigiLazzari/sudoku original com string.lua
@@ -0,0 +1,38 @@
+sudoku = " 1 2 3 4 5 6 7 8 9 " ..
+         " 4 5 6 7 8 9 1 2 3 " ..
+         " 7 8 9 1 2 3 4 5 6 " ..
+         " 8 9 1 2 3 4 5 6 7 " ..
+         " 2 3 4 5 6 7 8 9 1 " ..
+         " 5 6 7 8 9 1 2 3 4 " ..
+         " 6 7 8 9 1 2 3 4 5 " ..
+         " 9 1 2 3 4 5 6 7 8 " ..
+         " 3 4 5 6 7 8 9 1 2 " --deixando bonito para testes, inicializando o sudoku com um que é válido
+sudoku = sudoku:gsub(" ", ""); -- tira espaços /fail
+
+
+
+tables = {                 -- checando array, 1=linha, 2=coluna, 3=quadrantes
+            {}, {}, {}
+         } 
+
+for k = 1, 3 do
+    for l = 1, 9 do 
+        tables[k][l] = {0, 0, 0, 0, 0, 0, 0, 0, 0} -- enche o array com 0s (só pra não ter nils)
+    end 
+end 
+
+for k=1, 81 do -- loop por todas as posições
+    number = tonumber(sudoku:sub(k,k):match'%d') -- pega o caracter, checa se é um número e converte pra número (pq está como string)
+    if number then
+        crazy = { math.floor((k-1)/9)+1,(k-1)%9+1 } -- Pega o numero da linha e da coluna
+        crazy[3] = math.floor((crazy[1]-1)/3)+3*math.floor((crazy[2]-1)/3)+1 -- Pega o numero do quadrante
+        for l = 1,3 do v = tables[l][crazy[l]] -- 1 = linha, 2 = coluna, 3 = quadrante
+            if v[number] then -- não eliminados nessa coluna/linha/quadrante
+                v[number] = nil    
+            else
+                flag = 1 -- Inválido
+            end
+        end
+    end
+end
+io.write(flag and"Invalid"or"Valid")
--- Desafio_3/LuigiLazzari/troco.lua
+++ Desafio_3/LuigiLazzari/troco.lua
@@ -1,114 +1,43 @@
-t = {}
-setmetatable(t,{__index=function () return 0 end}) --inicializa todos os indexes da tabela t com 0 -- very useful <3 luv it
+local grana = { --Tabela q contém a informação das notas e moedas
+    { nome = 'Notas de R$ 100,00', valor = 100 },
+    { nome = 'Notas de R$ 50,00', valor = 50 },
+    { nome = 'Notas de R$ 20,00', valor = 20 },
+    { nome = 'Notas de R$ 10,00', valor = 10 },
+    { nome = 'Notas de R$ 5,00', valor = 5 },
+    { nome = 'Notas de R$ 2,00', valor = 2 },
+    { nome = 'Moedas de R$ 1,00', valor = 1 },
+    { nome = 'Moedas de R$ 0,50', valor = 0.5 },
+    { nome = 'Moedas de R$ 0,25', valor = 0.25 },
+    { nome = 'Moedas de R$ 0,10', valor = 0.1 },
+    { nome = 'Moedas de R$ 0,05', valor = 0.05 },
+}
 
-print("Preço produto: ")
-price = 15.25
-print("Valor pago: ")
-paid_price = 500
+--nome das funções já descreve, se tiver dúvida pergunta
 
-t.trocoReais = paid_price - price
-print(t.trocoReais)
-t.trocoCentavos = t.trocoReais * 100
-
-while(t.trocoCentavos % 10000 ~= t.trocoCentavos) do
-   t.nota100 = t.nota100 + 1
-   t.notas = t.notas + 1
-   t.trocoCentavos = t.trocoCentavos - 10000
-end
-if t.nota100 ~= 0 then
-   print("Notas de R$ 100,00: ", t.nota100)
-end
-
-while(t.trocoCentavos % 5000 ~= t.trocoCentavos) do
-   t.nota50 = t.nota50 + 1
-   t.notas = t.notas + 1
-   t.trocoCentavos = t.trocoCentavos - 5000
-end
-if t.nota50 ~= 0 then
-   print("Notas de R$ 50,00: ", t.nota50)
+local function contaQuantidadeResto(val, qntd)
+    return math.floor(qntd / val)
 end
 
-while(t.trocoCentavos % 2000 ~= t.trocoCentavos) do
-   t.nota20 = t.nota20 + 1
-   t.notas = t.notas + 1
-   t.trocoCentavos = t.trocoCentavos - 2000
-end
-if t.nota20 ~= 0 then
-   print("Notas de R$ 20,00: ", t.nota20)
+local function pegaRestoQuantidade(val, qntd)
+    return (qntd % val)
 end
 
-while(t.trocoCentavos % 1000 ~= t.trocoCentavos) do
-   t.nota10 = t.nota10 + 1
-   t.notas = t.notas + 1
-   t.trocoCentavos = t.trocoCentavos - 1000
-end
-if t.nota10 ~= 0 then
-   print("Notas de R$ 10,00: ", t.nota10)
+local function mostraValor(preco, pago, grana)
+    local resto = (pago - preco)
+    for _, dinheiro in ipairs(grana) do --[[ o _ significa q eu n vou usar a variável 
+                                             q estaria no for então eu declaro assim pq é de acordo com as convenções da lingiuagem
+                                             de acordo com documentação oficial: http://lua-users.org/wiki/LuaStyleGuide ]]--
+        qntd = contaQuantidadeResto(dinheiro.valor, resto)
+        resto = pegaRestoQuantidade(dinheiro.valor, resto)
+
+        if qntd > 0 then
+            print(dinheiro.nome .. ': ' .. qntd)
+        end
+    end
 end
 
-while(t.trocoCentavos % 500 ~= t.trocoCentavos) do
-   t.nota5 = t.nota5 + 1
-   t.notas = t.notas + 1
-   t.trocoCentavos = t.trocoCentavos - 500
-end
-if t.nota5 ~= 0 then
-   print("Notas de R$ 5,00: ", t.nota5)
-end
 
-while(t.trocoCentavos % 200 ~= t.trocoCentavos) do
-   t.nota2 = t.nota2 + 1
-   t.notas = t.notas + 1
-   t.trocoCentavos = t.trocoCentavos - 200
-end
-if t.nota2 ~= 0 then
-   print("Notas de R$ 2,00: ", t.nota2)
-end
+local preco = 15.25 -- se for usar o interpretador troque 15.25 por io.read(*number)
+local pago = 50.00 --mesma coisa do de cima
 
-print(string.format("------------\nQntd. total de notas >>>> %.0f\n------------", t.notas)); 
-
-while(t.trocoCentavos % 100 ~= t.trocoCentavos) do
-   t.moeda1 = t.moeda1 + 1
-   t.moedas = t.moedas + 1
-   t.trocoCentavos = t.trocoCentavos - 100
-end
-if t.moeda1 ~= 0 then
-   print("Moedas de R$ 1,00: ", t.moeda1)
-end
-
-while(t.trocoCentavos % 50 ~= t.trocoCentavos) do
-   t.moeda50 = t.moeda50 + 1
-   t.moedas = t.moedas + 1
-   t.trocoCentavos = t.trocoCentavos - 50
-end
-if t.moeda50 ~= 0 then
-   print("Moedas de R$ 0,50: ", t.moeda50)
-end
-
-while(t.trocoCentavos % 25 ~= t.trocoCentavos) do
-   t.moeda25 = t.moeda25 + 1
-   t.moedas = t.moedas + 1
-   t.trocoCentavos = t.trocoCentavos - 25
-end
-if t.moeda25 ~= 0 then
-   print("Moedas de R$ 0,25: ", t.moeda25)
-end
-
-while(t.trocoCentavos % 10 ~= t.trocoCentavos) do
-   t.moeda10 = t.moeda10 + 1
-   t.moedas = t.moedas + 1
-   t.trocoCentavos = t.trocoCentavos - 10
-end
-if t.moeda10 ~= 0 then
-   print("Moedas de R$ 0,10: ", t.moeda10)
-end
-
-while(t.trocoCentavos % 5 ~= t.trocoCentavos) do
-   t.moeda05 = t.moeda05 + 1
-   t.moedas = t.moedas + 1
-   t.trocoCentavos = t.trocoCentavos - 5
-end
-if t.moeda05 ~= 0 then
-   print("Moedas de R$ 0,05: ", t.moeda05)
-end
-
-print(string.format("------------\nQtd. total de moedas >>>> %.0f\n------------", t.moedas)); 
+mostraValor(preco, pago, grana)
